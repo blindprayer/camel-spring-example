9e23ae3731c5b6cdd0653866cc73d61f25557def
blindprayer <blind_prayer@naver.com>
2018-05-21 17:24:21 +0900

    Init Commit

---
+++ src/main/java/com/javacodegeeks/camel/SpringImportRouteExample.java
@@ -0,0 +1,23 @@
+package com.javacodegeeks.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringImportRouteExample {
+	public static final void main(String[] args) throws Exception {
+		ConfigurableApplicationContext appContext = new ClassPathXmlApplicationContext(
+				"importRouteApplicationContext.xml");
+/*		CamelContext camelContext = SpringCamelContext.springCamelContext(
+				appContext, false);*/
+		CamelContext camelContext = new SpringCamelContext(appContext);
+		try {
+			camelContext.start();
+			Thread.sleep(3000);
+		} finally {
+			camelContext.stop();
+			appContext.close();
+		}
+	}
+}
